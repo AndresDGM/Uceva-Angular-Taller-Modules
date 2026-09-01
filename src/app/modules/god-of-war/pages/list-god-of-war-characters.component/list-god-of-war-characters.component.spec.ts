f61de8ce1bc4b23c7b0bd18acf98467c1a9f191f
gutinico023-dev <gutinico023@gmail.com>
2026-08-31 19:30:03 -0500

    test: completar pruebas del módulo God of War

--- src/app/modules/god-of-war/pages/list-god-of-war-characters.component/list-god-of-war-characters.component.spec.ts
+++ src/app/modules/god-of-war/pages/list-god-of-war-characters.component/list-god-of-war-characters.component.spec.ts
@@ -1,23 +1,81 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of, throwError } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
-import { ListGodOfWarCharactersComponent } from './list-god-of-war-characters.component';
+import { GOD_OF_WAR_CHARACTERS } from '../../../../core/config/god-of-war.config';
+import { TableGodOfWarCharactersComponent } from '../../components/table-god-of-war-characters.component/table-god-of-war-characters.component';
+import { GodOfWarCharactersService } from '../../services/god-of-war-characters.service';
+import { ListGodOfWarCharacters } from './list-god-of-war-characters.component';
 
 describe('ListGodOfWarCharactersComponent', () => {
-  let component: ListGodOfWarCharactersComponent;
-  let fixture: ComponentFixture<ListGodOfWarCharactersComponent>;
+  let component: ListGodOfWarCharacters;
+  let fixture: ComponentFixture<ListGodOfWarCharacters>;
+  let godOfWarCharactersService: GodOfWarCharactersService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ListGodOfWarCharactersComponent]
+      declarations: [ListGodOfWarCharacters, TableGodOfWarCharactersComponent],
+      imports: [CommonModule]
     })
-    .compileComponents();
+      .compileComponents();
 
-    fixture = TestBed.createComponent(ListGodOfWarCharactersComponent);
+    fixture = TestBed.createComponent(ListGodOfWarCharacters);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    godOfWarCharactersService = TestBed.inject(GodOfWarCharactersService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería llamar a getAllCharacters al iniciar', () => {
+    const spyGetAllCharacters = jest
+      .spyOn(godOfWarCharactersService, 'getAllCharacters')
+      .mockReturnValue(of(GOD_OF_WAR_CHARACTERS));
+
+    fixture.detectChanges();
+
+    expect(spyGetAllCharacters).toHaveBeenCalled();
+  });
+
+  it('debería asignar los personajes recibidos del servicio', () => {
+    jest
+      .spyOn(godOfWarCharactersService, 'getAllCharacters')
+      .mockReturnValue(of(GOD_OF_WAR_CHARACTERS));
+
+    fixture.detectChanges();
+
+    expect(component.characters).toEqual(GOD_OF_WAR_CHARACTERS);
+  });
+
+  it('debería pasar los personajes al componente table-god-of-war-characters', () => {
+    jest
+      .spyOn(godOfWarCharactersService, 'getAllCharacters')
+      .mockReturnValue(of(GOD_OF_WAR_CHARACTERS));
+
+    fixture.detectChanges();
+
+    const tableComponent = fixture.debugElement
+      .query(By.directive(TableGodOfWarCharactersComponent))
+      .componentInstance;
+
+    expect(tableComponent.characters).toEqual(GOD_OF_WAR_CHARACTERS);
+  });
+
+  it('debería manejar el error cuando falla getAllCharacters', () => {
+    component.characters = [];
+    const errorResponse = new Error('Error al cargar personajes');
+
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+    jest
+      .spyOn(godOfWarCharactersService, 'getAllCharacters')
+      .mockReturnValue(throwError(() => errorResponse));
+
+    fixture.detectChanges();
+
+    expect(godOfWarCharactersService.getAllCharacters).toHaveBeenCalled();
+    expect(console.error).toHaveBeenCalledWith(errorResponse);
+    expect(component.characters.length).toBe(0);
   });
 });
--- src/app/modules/god-of-war/services/god-of-war-characters.service.spec.ts
+++ src/app/modules/god-of-war/services/god-of-war-characters.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { GOD_OF_WAR_CHARACTERS } from '../../../core/config/god-of-war.config';
 import { GodOfWarCharactersService } from './god-of-war-characters.service';
 
 describe('GodOfWarCharactersService', () => {
@@ -11,5 +12,13 @@ describe('GodOfWarCharactersService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getAllCharacters debería retornar un observable con los personajes', (done) => {
+    service.getAllCharacters().subscribe(characters => {
+      expect(characters).toEqual(GOD_OF_WAR_CHARACTERS);
+      expect(characters.length).toBe(GOD_OF_WAR_CHARACTERS.length);
+      done();
+    });
   });
 });
